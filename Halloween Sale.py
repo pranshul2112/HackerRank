3c08db0e2d7bcd9dd88908a74f1b1cd1f3406bc1
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-09 22:16:29 +0530

    Create Halloween Sale.py

---
+++ Halloween Sale.py
@@ -0,0 +1,12 @@
+def halloweenSale(p, d, m, s):
+    flag = 0
+    while s >= p:
+        
+        flag += 1
+        s -= p
+        p = max(p - d, m)
+    
+    
+    return flag
+p, d, m, s = map(int, input().split())
+print(halloweenSale(p, d, m, s))
