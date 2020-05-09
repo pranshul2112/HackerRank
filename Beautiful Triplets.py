0ec7798fe1b9eec54a62e5d3055c7dbc888d8652
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-09 21:46:30 +0530

    Create Beautiful Triplets.py

---
+++ Beautiful Triplets.py
@@ -0,0 +1,14 @@
+
+def beautifultriplets(arr, n, d):
+    count =  0
+    for i in range(n):
+        if arr[i] + d in arr and arr[i] + (2 * d) in arr :
+                count += 1
+    return count
+
+nd = list(map(int, input().split()))
+n = nd[0]
+d = nd[1]
+arr = list(map(int, input().rstrip().split()))
+res = beautifultriplets(arr, n, d)
+print(res)
