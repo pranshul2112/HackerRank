8f317123db566804dddf6e357fee18d41461dfac
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-09 23:02:22 +0530

    Create Taum And B'day.py

---
+++ Taum And B'day.py
@@ -0,0 +1,15 @@
+def taumBday(b, w, bc, wc, z):
+    if bc > wc and wc + z < bc: #Converting Black Gifts into White
+        return b*wc + w*wc + b*z
+    elif wc > bc and bc + z < wc: #Converting White Gifts into Black
+        return b*bc + w*bc + w*z
+
+    return b*bc + w*wc   
+
+
+
+t = int(input())
+for t_tr in range (t):
+    black, white = map(int, input().split())
+    bc, wc, z = map(int, input().split())
+    print(taumBday(black, white, bc, wc, z))
