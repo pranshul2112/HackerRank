873fcfb40b15dc56d731712c3b9e1f007e0df3f0
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-10 22:23:34 +0530

    Create Maximum Subarray Sum.py

---
+++ Maximum Subarray Sum.py
@@ -0,0 +1,28 @@
+
+from bisect import insort, bisect
+
+
+def maximumSum(arr, m):
+    gsum = lsum = 0
+    
+    psum = []
+
+    for i in range(len(arr)):
+        lsum = (lsum + arr[i]) % m
+        pos = bisect(psum, lsum)
+        
+        x = 0 if pos == i else psum[pos]
+        
+        gsum = max(gsum, (lsum - x + m) % m)
+        
+        insort(psum, lsum)
+    
+    
+    return gsum
+
+
+q = int(input())
+for i in range(q):
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    print(maximumSum(arr, m))
