68c33b1094b643921263837ff7198cb2db953b61
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-16 00:56:53 +0530

    Delete Electronics Shop.py

--- Electronics Shop.py
+++
@@ -1,9 +0,0 @@
-def designerPDF(keyboards, drives, b):
-    return max([sum([x, y]) for x in keyboards for y in drives if sum([x, y]) <= b] + [-1])
-
-
-b, n, m = map(int, input().split())
-keyboards = list(map(int, input().rstrip().split()))
-drives = list(map(int, input().rstrip().split()))
-get_money = designerPDF(keyboards, drives, b)
-print(get_money)
