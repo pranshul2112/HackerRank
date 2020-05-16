6b9ad65d809a6983e5a80e1fd0763761ead39959
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-17 00:23:00 +0530

    Create Sherlock And Array.py

---
+++ Algorithms/Search/Sherlock And Array.py
@@ -0,0 +1,25 @@
+
+def balancedSums(arr):
+    check = arr[0]
+    n = len(arr)
+    s = sum(arr)
+    if s - check == 0:
+        return "YES"
+
+    for i in range(1, n):
+        if s - arr[i] - check == check or s - arr[i] == 0:
+            return "YES"
+
+        check += arr[i]
+
+    return "NO"
+
+
+T = int(input().strip())
+
+for T_itr in range(T):
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(balancedSums(arr))
+    
