ae904423c68e1ef0b8a5b1f9c5823dae83c5c811
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-16 23:59:03 +0530

    Create Modified Kaprekar Numbers.py

---
+++ Algorithms/Implementation/Modified Kaprekar Numbers.py
@@ -0,0 +1,20 @@
+
+p = int(input())
+q = int(input())
+
+result=[]
+for i in range(p,q+1,1):
+    k = i**2
+    right = k % (10 ** len(str(i)))
+    left = k // (10 ** len(str(i)))
+    if i == int(right)+int(left):
+            result.append(i)
+ 
+
+
+result= list(set(result))
+result.sort()
+if len(result) == 0:
+    print ('INVALID RANGE')
+else:
+    print (' '.join([str(i) for i in result]))
