0a76335503e9cbaff49a82f9cb788437994adf5f
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-17 00:07:19 +0530

    Create Manasa And Stones.py

---
+++ Algorithms/Implementation/Manasa And Stones.py
@@ -0,0 +1,16 @@
+
+def manasa(n, a, b):
+    s = set() # Stores data in unsorted form
+    for i in range(n):
+        s.add(i * a + (n - i - 1)* b)
+    s = list(s)
+    s.sort()
+    return " ".join([str(x) for x in s])
+
+t = int(input())
+for t_tr in range(t):
+    n = int(input())
+    a = int(input())
+    b = int(input())
+    print(manasa(n, a, b))
+    
