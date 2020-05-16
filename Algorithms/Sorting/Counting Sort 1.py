ff6b32e34a174b15ac31d62140de189a97c16e42
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-16 23:16:09 +0530

    Create Counting Sort 1.py

---
+++ Algorithms/Sorting/Counting Sort 1.py
@@ -0,0 +1,18 @@
+
+def solution(data, n):
+    dic = {}
+
+    for i in data:
+        dic[i] = dic.get(i, 0) + 1
+
+    for key in range(100):
+            if key in dic:
+                print(dic[key], end=' ')
+            else:
+                print(0, end=' ')
+
+    return None
+
+n = int(input())
+data = list(map(int, input().split()))
+(solution(data, n))
