d8bd651b1df765913d4d326ab9bbc1a4f69b9bad
pranshul2112 <63261270+pranshul2112@users.noreply.github.com>
2020-05-29 22:46:10 +0530

    Create The Love-Letter Mystery.py

---
+++ Algorithms/Sorting/The Love-Letter Mystery.py
@@ -0,0 +1,22 @@
+def mincost(string):
+    n = len(string)
+    res = 0
+    
+    for i in range(n // 2):
+        x = ord(string[i])
+        y = ord(string[n - i - 1])
+        
+        if x == y:
+            continue
+            
+        else:
+            res += abs(x - y)
+            
+    return res
+
+
+n = int(input())
+for _ in range(n):
+    string = input()
+    print(mincost(string))
+    
